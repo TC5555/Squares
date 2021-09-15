07086bf1f976a6519bfebc1f78d882b3228e25f0
Tyler Carter <70717266+TC5555@users.noreply.github.com>
2021-09-15 10:53:01 -0700

    made it so it could be done with no multiplication.

--- src/squres.java
+++ src/squres.java
@@ -1,7 +1,12 @@
 public class squres {
     public static void main(String[] args) {
         for (int j = 1; j <= 10; j++) {
-            System.out.print((j*j) + " ");
+            int i = j;
+            int a = j;
+            for (int k = 2; k <= a; k++) {
+                i = i + a;
+            }
+            System.out.print(i + " ");
         }
     }
 }
